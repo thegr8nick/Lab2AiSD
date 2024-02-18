f04c24181afe62c6cf775c9c747e8189ba0184f0
thegr8nick <kudashevnikitushka@yandex.ru>
2024-02-19 00:33:37 +0400

    bintree

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/nikku/Desktop/f/huina
+# Install script for directory: C:/Users/nikku/Desktop/Lab1AiSD
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/nikku/Desktop/f/huina/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Mylist")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/nikku/Desktop/f/huina/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/nikku/Desktop/Lab1AiSD/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
